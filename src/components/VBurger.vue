b39fff2fffc1e0f5dded2c88b04a736c7f9f28d4
Pasha Zubak <pashazubak@gmail.com>
2020-05-31 12:12:05 +0300

    Add burger button

---
+++ src/components/VBurger.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="burger" :class="{ open: isOpen }" @click="isOpen = !isOpen">
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "VBurger",
+        data() {
+            return {
+                isOpen: false,
+                title: "Add contact"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
