d2e5d311f4729414eb33fb6d9ecc022be285f062
Pasha Zubak <pashazubak@gmail.com>
2020-06-10 01:49:01 +0300

    Add basic form component

---
+++ src/layouts/VForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form :class="vclass">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "VForm",
+        props: {
+            vclass: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
