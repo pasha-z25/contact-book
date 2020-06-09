ea57a58078d41ed2c2ab16f4f913d31b8f037170
Pasha Zubak <pashazubak@gmail.com>
2020-06-10 01:48:14 +0300

    Add i18n buttons for change lang

---
+++ src/components/SetLang.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flag-wrap">
+        <a href="#" class="link" @click="setLocale('en')"><flag iso="us"></flag></a>
+        <a href="#" class="link" @click="setLocale('ru')"><flag iso="ru"></flag></a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SetLang",
+        methods: {
+            setLocale(lang) {
+                import(`../langs/${lang}.json`).then( msg => {
+                    this.$i18n.setLocaleMessage(lang, msg);
+                    this.$i18n.locale = lang;
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .flag-wrap {
+        float: right;
+        align-self: flex-end;
+        padding: 5px;
+    }
+    .link {
+        margin: 0 5px;
+    }
+</style>
