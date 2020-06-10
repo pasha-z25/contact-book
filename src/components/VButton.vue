2767627a40f41d7109ea2bfed4eef97d2ddb135a
Pavel Zubak <front2.sofona@gmail.com>
2020-06-10 11:52:19 +0300

    Fixed default button

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -1,17 +1,36 @@
 <template>
     <button
-            :id="[id ? id : '']"
-            :type="[type ? type : 'submit']"
-            :class="[vclass ? vclass : '', 'btn']"
+            :id="id"
+            :type="type"
+            :class="vclass"
+
     >
-        <span :class="{ text : name }">{{ name }}</span>
+        <span class="text">
+            <slot> {{ name }}</slot>
+        </span>
     </button>
 </template>
 
 <script>
     export default {
         name: "VButton",
-        props: ['id', 'type', 'name', 'vclass']
+        props: {
+            id: {
+                type: [String, Number],
+            },
+            type: {
+                type: String,
+                default: 'submit'
+            },
+            name: {
+                type: String,
+                default: 'Send'
+            },
+            vclass: {
+                type: String,
+                default: 'btn'
+            }
+        }
     }
 </script>
 
