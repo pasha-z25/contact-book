e64090fb69af3c50c8d9a99ae5f8b6d95692d6dd
Pasha Zubak <pashazubak@gmail.com>
2020-06-16 23:13:21 +0300

    Add user actions

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,14 +1,57 @@
 export default {
     state: {
-
+        auth: false,
+        cookie: {},
+        user: {}
     },
     getters: {
-
+        getUser(state) {
+            return state.user;
+        }
     },
     mutations: {
-
+        setCookie(state, cookie) {
+            state.cookie = cookie;
+            document.cookie = `${state.cookie.name}=${state.cookie.value}`;
+        }
     },
     actions: {
-
-    }
+        async fetchUser(ctx, { email, password }) {
+            console.log(ctx);
+            const result = await fetch('/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'email': email,
+                    'password': password
+                })
+            });
+            result.then(response => response.json())
+                .catch(console.log)
+                .then( data => {
+                    ctx.commit('setCookie', data.cookie);
+                    fetch('/api/phonebook', {
+                        method: 'GET',
+                        mode: 'cors',
+                        credentials: 'include',
+                        withCredentials: true,
+                        headers: {
+                            'Content-type': 'application/json',
+                        },
+                    }).then(response => response.json())
+                        .catch(
+                            console.log,
+                        )
+                        .then(data => {
+                            console.log(data);
+                        });
+                    setTimeout(() => {
+                        this.$router.push = ("/register")
+                    }, 5000)
+                })
+        }
+    },
+    strict: process.env.NODE_ENV !== 'production'
 }
