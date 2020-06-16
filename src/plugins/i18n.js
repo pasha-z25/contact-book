c81291ff2b5bac0392a95c2344f93620f9a82a51
Pasha Zubak <pashazubak@gmail.com>
2020-06-16 21:43:31 +0300

    Fixed i18n plugin (multilanguage)

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from '../langs/en'
+import ru from '../langs/ru'
 
 Vue.use(VueI18n);
 
@@ -8,6 +9,7 @@ export const i18n = new VueI18n({
     locale: 'en',
     fallbackLocale: 'ru',
     messages: {
-        en
+        en,
+        ru
     }
 });
