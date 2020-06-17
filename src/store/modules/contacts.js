dba1af77634ca138e465461d0f05f14a00388e32
Pasha Zubak <pashazubak@gmail.com>
2020-06-18 00:18:06 +0300

    Add categories and contacts

--- src/store/modules/contacts.js
+++ src/store/modules/contacts.js
@@ -1,12 +1,20 @@
 export default {
     state: {
-
+        categories: [],
+        contacts: []
     },
     getters: {
-
+        getContacts(state) {
+            return state.contacts;
+        },
+        getContactsLength(state) {
+            return state.contacts.length;
+        }
     },
     mutations: {
-
+        addContacts(state, payload) {
+            state.contacts = state.contacts.concat(payload)
+        }
     },
     actions: {
 
