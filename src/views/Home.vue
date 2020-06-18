1862390becdf1f4821eb081432030951857728eb
Pasha Zubak <pashazubak@gmail.com>
2020-06-18 23:55:21 +0300

    Add contact view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
      <application :title="this.title">
          <div v-if="contactsLength">
              <h2>home page</h2>
+             <div v-for="contact in contacts" :key="contact.id">
+                 {{ contact }}
+             </div>
              <p>contactsLength {{ contactsLength }}</p>
              <p>categoriesLength {{ categoriesLength }}</p>
          </div>
@@ -28,9 +31,13 @@
         },
         computed: {
             ...mapGetters([
+                'getContacts',
                 'getContactsLength',
                 'getCategoriesLength'
             ]),
+            contacts() {
+                return this.getContacts
+            },
             contactsLength() {
                 return this.getContactsLength
             },
