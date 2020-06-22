bdaaef9bd712028ad088c663d109dccc5cd00c13
Pavel Zubak <front2.sofona@gmail.com>
2020-06-22 09:59:15 +0300

    Fixed ID props

--- src/components/VDropDown.vue
+++ src/components/VDropDown.vue
@@ -2,7 +2,7 @@
     <div class="dropdown">
         <p class="link" @click="isOpen = !isOpen"><span class="point"></span><span class="point"></span><span class="point"></span></p>
         <ul class="dropdown-menu" :class="{ open: isOpen }">
-            <li class="dropdown-item"><span class="dropdown-link">View</span></li>
+            <li class="dropdown-item"><router-link :to=link class="dropdown-link">View</router-link></li>
             <li class="dropdown-item"><router-link :to=link class="dropdown-link">Edit</router-link></li>
             <li class="dropdown-item"><span class="dropdown-link">Delete</span></li>
         </ul>
