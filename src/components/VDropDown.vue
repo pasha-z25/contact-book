7e122f248359bce4504ad584670d34092b9b4657
Pasha Zubak <pashazubak@gmail.com>
2020-06-23 23:05:27 +0300

    Add new action

--- src/components/VDropDown.vue
+++ src/components/VDropDown.vue
@@ -2,8 +2,8 @@
     <div class="dropdown">
         <p class="link" @click="isOpen = !isOpen"><span class="point"></span><span class="point"></span><span class="point"></span></p>
         <ul class="dropdown-menu" :class="{ open: isOpen }">
-            <li class="dropdown-item"><router-link :to=link class="dropdown-link">View</router-link></li>
-            <li class="dropdown-item"><router-link :to=link class="dropdown-link">Edit</router-link></li>
+            <li class="dropdown-item"><router-link :to=linkView class="dropdown-link" @click.native="getFullContact({id})">View</router-link></li>
+            <li class="dropdown-item"><router-link :to=linkEdit class="dropdown-link" @click.native="getFullContact({id})">Edit</router-link></li>
             <li class="dropdown-item"><span class="dropdown-link">Delete</span></li>
         </ul>
         <slot></slot>
@@ -20,8 +20,17 @@
             }
         },
         computed: {
-            link({id}) {
+            linkEdit({id}) {
                 return `/edit/${id}`
+            },
+            linkView({id}) {
+                return `/view/${id}`
+            }
+        },
+        methods: {
+            getFullContact({id}) {
+                console.log('ID: ', id);
+                this.$store.dispatch('fullInfoContact', id);
             }
         }
     }
--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -1,10 +1,41 @@
 <template>
-    <div></div>
+    <application :title="this.title">
+        <div>
+            {{ contact }}
+        </div>
+        <div>
+            <button @click="contact2">new</button>
+            <p>{{ contact2 }}</p>
+        </div>
+    </application>
 </template>
 
 <script>
+    import { mapGetters } from "vuex"
+    import Application from "../layouts/Application";
     export default {
-        name: "ViewContact"
+        name: "ViewContact",
+        components: {
+            Application
+        },
+        data() {
+            return {
+                title: "View current user"
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'getCurrentContact',
+            ]),
+            contact() {
+                return this.getCurrentContact
+            },
+        },
+        methods: {
+            contact2() {
+                return this.getCurrentContact
+            },
+        }
     }
 </script>
 
