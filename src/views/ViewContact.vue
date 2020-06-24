cac80ad9db0e8e7d64bbba92974fc29aa79b5fed
Pasha Zubak <pashazubak@gmail.com>
2020-06-24 23:29:36 +0300

    Fixed view page

--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -1,11 +1,14 @@
 <template>
     <application :title="this.title">
         <div>
-            {{ contact }}
-        </div>
-        <div>
-            <button @click="contact2">new</button>
-            <p>{{ contact2 }}</p>
+            <p><b>Name</b> <span>{{ contact.name }}</span></p>
+            <p><b>Surname</b> <span>{{ contact.surname }}</span></p>
+            <p><b>Phone</b> <span>{{ contact.phone[0].value }}</span></p>
+            <p><b>Email</b> <span>{{ contact.email[0] }}</span></p>
+            <p><b>Born Date</b> <span>{{ contact.bornDate | cutDate }}</span></p>
+            <p><b>Category</b> <span>{{ contact.phone[0].category | capitalize }}</span></p>
+            <p v-if="contact.position"><b>Position</b> <span>{{ contact.position }}</span></p>
+            <p v-if="contact.information"><b>Information</b> <span>{{ contact.information }}</span></p>
         </div>
     </application>
 </template>
@@ -31,10 +34,15 @@
                 return this.getCurrentContact
             },
         },
-        methods: {
-            contact2() {
-                return this.getCurrentContact
+        filters: {
+            cutDate(value) {
+                return value.substring(0, 10)
             },
+            capitalize(value) {
+                if (!value) return '';
+                value = value.toString();
+                return value.charAt(0).toUpperCase() + value.slice(1)
+            }
         }
     }
 </script>
