70daa5dec498613c8654cbcc2a8c61dcfa2b4122
Pasha <pashazubak@gmail.com>
2020-12-05 01:04:05 +0200

    remove actions to store

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,71 +1,64 @@
 <template>
   <authorization>
-    <h1 class="title text-center"><span class="big">Заполните</span> <br>данные для регистрации.</h1>
-    <form action="" class="form d-flex flex-wrap justify-content-center position-center">
-      <input type="email" class="input email" placeholder="E-mail" required v-model="email">
-      <input type="text" class="input" placeholder="Name" required v-model="name">
-      <input type="text" class="input" placeholder="Surname" required v-model="surname">
-      <input type="password" class="input" placeholder="Password" required v-model="password">
-      <input type="password" class="input" placeholder="Repeat Password" required v-model="repassword">
-      <v-button type="submit" class="submit" @click="handleClick">{{ $t('btnLogIn') }}</v-button>
+    <h1 class="title text-center"><span class="big">{{ $t('msgFillIn') }}</span> <br>{{ $t('msgRegistrationData') }}.</h1>
+    <form @submit.prevent="handleClick(user)" class="form d-flex flex-wrap justify-content-center position-center">
+      <div class="input-group email">
+        <label class="label">
+          <input type="email" class="input email" placeholder="E-mail" required v-model="user.email">
+        </label>
+      </div>
+      <div class="input-group">
+        <label class="label">
+          <input type="text" class="input" placeholder="Name" required v-model="user.name">
+        </label>
+      </div>
+      <div class="input-group">
+        <label class="label">
+          <input type="text" class="input" placeholder="Surname" required v-model="user.surname">
+        </label>
+      </div>
+      <div class="input-group">
+        <label class="label">
+          <input type="password" class="input" placeholder="Password" required v-model="user.password">
+        </label>
+      </div>
+      <div class="input-group">
+        <label class="label">
+          <input type="password" class="input" placeholder="Repeat Password" required v-model="user.repassword">
+        </label>
+      </div>
+      <button type="submit" class="btn btn-orange submit" @click.prevent="handleClick(user)">{{ $t('btnRegister') }}</button>
     </form>
-    <p class="text text-center">Есть аккаунт? <router-link to="/" class="link">Авторизируйтесь.</router-link></p>
+    <p class="text text-center">{{ $t('msgHaveAccount') }} <router-link to="/" class="link">{{ $t('msgLogIn') }}.</router-link></p>
   </authorization>
 </template>
 
 <script>
   import Authorization from "../layouts/Authorization";
-  import routes from "../routes";
-  import VButton from "../components/VButton";
 
   export default {
     name: "Register",
     components: {
-      VButton,
       Authorization
     },
     data() {
       return {
-        email: '',
-        name: '',
-        surname: '',
-        password: '',
-        repassword: '',
+        user: {
+          email: '',
+          name: '',
+          surname: '',
+          password: '',
+          repassword: '',
+        }
       }
     },
     methods: {
-      handleClick(e) {
-        e.preventDefault();
+      handleClick(user) {
         (this.password === this.repassword)
         ? (
-          fetch('/api/users/register', {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-              'email': this.email,
-              'password': this.password,
-              'name': this.name,
-              'surname': this.surname
-            })
-          }).then(response => response.json())
-                  .catch(console.log)
-                  .then(data => {
-                    console.log(data.message);
-                    setTimeout(() => {
-                      this.$root.currentRoute = '/';
-                      window.history.pushState(
-                              null,
-                              routes['/'],
-                              '/'
-                      )
-                    }, 2000)
-                  })
+            this.$store.dispatch('registerUser', user)
           )
         : alert('Пароли не совпадают');
-        console.log('auth ', this.email, this.password)
-
       }
     },
   }
@@ -80,7 +73,11 @@
   .title, .text {
     margin-bottom: calc(var(--indent-default) * 2);
   }
-  .input, .submit {
+  .input-group {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .input-group, .input, .submit {
     width: 220px;
     font-size: 16px;
   }
@@ -90,11 +87,12 @@
     background-color: transparent;
     margin-bottom: var(--indent-default);
     padding: 5px;
-    margin-left: auto;
-    margin-right: auto;
     color: inherit;
   }
-  .input.email {
+  .input::placeholder {
+    color: var(--color-lighgtext);
+  }
+  .input.email, .input-group.email {
     width: 100%;
     max-width: 470px;
   }
